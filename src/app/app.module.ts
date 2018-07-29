20ce37e47a51f0f823801db9ba2192b1f46c47fd
Bhaskar <bhaskarghosh16@icloud.com>
2018-07-29 21:14:25 +0530

    Angular 6 demo application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+//import { MaterialModule } from '@angular/material'; -- This is deprecated
 
+import {
+  MatButtonModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule,
+  MatListModule,
+  MatGridListModule
+} from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+
+//import hammerjs;
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatListModule,
+    FlexLayoutModule,
+    MatGridListModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
