73948b54c515bead0c5c826d2bdd5e25f93c43ea
Bhaskar <bhaskarghosh16@icloud.com>
2018-08-01 14:34:03 +0530

    Header footer components segregated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,12 +20,16 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import hammerjs;
 
 import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
