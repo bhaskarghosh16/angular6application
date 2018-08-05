67efa260a6c9d5370f20ac32d7846a2646ea4fde
Bhaskar <bhaskarghosh16@icloud.com>
2018-08-05 21:21:21 +0530

    SPA 1 with routing params

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,5 +1,10 @@
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 import { Component, OnInit,Input } from '@angular/core';
 import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
+
+
 
 
 @Component({
@@ -7,14 +12,22 @@ import { Dish } from '../shared/dish';
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss']
 })
-export class DishdetailComponent implements OnInit {
-  @Input()
-  dish:Dish;
-  //comments = DISH.comments;
 
-  constructor() { }
+export class DishdetailComponent implements OnInit {
+
+  dish: Dish;
+
+  constructor(private dishservice: DishService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit() {
+    const id = +this.route.snapshot.params['id'];
+    this.dish = this.dishservice.getDish(id);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
